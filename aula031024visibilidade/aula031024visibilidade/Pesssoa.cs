d00bdf738401379cb884572f0d747a9e0ba29d8a
Abraão Filipi dos Santos <107932922+abraaosantosdeveloper@users.noreply.github.com>
2024-10-03 16:08:20 -0300

    Add files via upload

---
+++ aula031024visibilidade/aula031024visibilidade/Pesssoa.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula031024visibilidade
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string telefone;
+        private string endereco;
+        private string email;
+
+        /*        public void setIdade(int valor)
+        /*        {
+                    if (valor > 0)
+                    {
+                        this.idade = valor;
+                        Console.WriteLine($"Idade atual: {this.idade}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"A idade inserida ({valor}) é inválida.");
+                    }
+                }*/
+
+/*        public void setNome(string novo_nome) { 
+            if (nome == "")
+            {
+                Console.WriteLine("O nome inserido é inválido");
+            }
+            else
+            {
+                this.nome = novo_nome;
+                Console.WriteLine(this.nome);
+            }
+        }*/
+
+        public string Nome
+        {
+            get { return (string) nome; }
+            set { if (string.IsNullOrWhiteSpace(value)) this.nome = value; else Console.WriteLine("Nome inválido"); }
+        }
+
+        public int Idade
+        {
+            get { return (int)(idade * 0.7); }
+            set { if (value > 0) this.idade = value;  }
+        }
+
+        public string Telefone
+        {
+            get { return (string)telefone; }
+            set { if (string.IsNullOrWhiteSpace(value)) this.telefone = value; else Console.WriteLine("Telefone inválido"); }
+        }
+
+        public string Endereco
+        {
+            get { return (string)endereco; }
+            set { if (string.IsNullOrWhiteSpace(value)) this.endereco = value; else Console.WriteLine("Email inválido"); }
+        }
+
+        public string Email
+        {
+            get { return (string)email; }
+            set { if (string.IsNullOrWhiteSpace(value)) this.email = value; else Console.WriteLine("Email inválido"); }
+        }
+
+    }
+}
---
+++ aula031024visibilidade/aula031024visibilidade/Program.cs
@@ -0,0 +1,45 @@
+﻿// Tipos de visibilidade: public, private, internal, protected, protected internal
+
+/*
+Console.Write("Insira o nome: ");
+string novo_nome = Console.ReadLine();
+Console.Write("Insira a idade: ");
+int valor = int.Parse(Console.ReadLine());
+
+p.setNome(novo_nome); 
+p.Idade = valor;
+
+Console.WriteLine(p.Idade);*/
+
+
+using aula031024visibilidade;
+
+Pessoa p = new Pessoa();
+
+Console.Write("Digite o Nome: ");
+p.Nome = Console.ReadLine();
+
+
+Console.Write("Digite o idade: ");
+p.Idade = int.Parse(Console.ReadLine());
+
+
+Console.Write("Digite o telefone: ");
+p.Telefone = Console.ReadLine();
+
+
+Console.Write("Digite o endereço: ");
+p.Endereco = Console.ReadLine();
+
+
+Console.Write("Digite o Email: ");
+p.Email = Console.ReadLine();
+
+Console.WriteLine("===================================================");
+
+Console.WriteLine("Informações cadastradas do usuário: ");
+Console.WriteLine(p.Nome);
+Console.WriteLine(p.Idade);
+Console.WriteLine(p.Telefone);
+Console.WriteLine(p.Endereco);
+Console.WriteLine(p.Email);
